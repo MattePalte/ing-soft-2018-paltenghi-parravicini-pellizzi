5295a29a620339b3c43075bfacb18644652060d6
Matteo Paltenghi <MattePalte@users.noreply.github.com>
2018-03-20 15:44:07 +0100

    Initial commit

---
+++ src/main/java/provaGr/App.java
@@ -0,0 +1,13 @@
+package provaGr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
