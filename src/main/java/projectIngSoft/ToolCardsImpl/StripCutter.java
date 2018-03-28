72d2d740f97612d615ad78f43ca1c631d433ea84
parx <daniparra@hotmail.it>
2018-03-28 22:05:17 +0200

    Modified "actual" ToolCards (added name, descriptions, colour for solo play). Slighty modified cards in order to accept description as a default parameter. Aggiunto taglia strice (carta in inglese di dubbio utilizzo)

--- src/main/java/projectIngSoft/ToolCardsImpl/StripCutter.java
+++ src/main/java/projectIngSoft/ToolCardsImpl/StripCutter.java
@@ -1,9 +1,18 @@
 package projectIngSoft.ToolCardsImpl;
 
+import projectIngSoft.Colour;
 import projectIngSoft.ToolCard;
 import projectIngSoft.WindowFrame;
 
+import java.awt.*;
+
 public class StripCutter extends ToolCard {
+
+    public StripCutter() {
+        super("Taglia strisce", "Prendi un dado da qualsiasi altro giocatore. Al suo posto dai loro un dado dello stesso colore o dello stesso valore\n" +
+                "Possono piazzare il dado senza curarsi delle restrizioni di colore o valore\n" +
+                "Deve essere usato prima del Round 7", Colour.BLANK);
+    }
 
     public void applyEffect(WindowFrame window) {
 
