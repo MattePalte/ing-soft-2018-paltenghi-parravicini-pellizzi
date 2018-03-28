0aafccdfd4dd8cb9d28b62cbc3b3489eaedf66e1
krijo <10522862@polimi.it>
2018-03-28 16:47:37 +0200

    Added SfumatureMedie.java SfumatureScure.java

---
+++ src/main/java/projectIngSoft/PublicObjectiveImpl/SfumatureMedie.java
@@ -0,0 +1,14 @@
+package projectIngSoft.PublicObjectiveImpl;
+
+import projectIngSoft.PublicObjective;
+
+public class SfumatureMedie extends PublicObjective{
+
+    public SfumatureMedie(){
+        super("Sfumature Medie", "Conta il numero di coppie di dadi (3,4) presenti sulla tua vetrata", 2);
+    };
+
+    public void countPoints() {
+
+    }
+}
---
+++ src/main/java/projectIngSoft/PublicObjectiveImpl/SfumatureScure.java
@@ -0,0 +1,14 @@
+package projectIngSoft.PublicObjectiveImpl;
+
+import projectIngSoft.PublicObjective;
+
+public class SfumatureScure extends PublicObjective {
+
+    public SfumatureScure(){
+        super("Sfumature Scure", "Conta il numero di coppie di dadi (5,6) presenti sulla tua vetrata", 2);
+    }
+
+    public void countPoints() {
+
+    }
+}
