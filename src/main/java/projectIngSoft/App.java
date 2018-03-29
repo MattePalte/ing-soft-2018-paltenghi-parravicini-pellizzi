d731a092b9f29ab53b09278a55078399fb600ab9
MattePalte <mattepalte@live.it>
2018-03-29 23:11:36 +0200

    Player class added  and App and Game classes edited (first phases of the game setup added) Problem with getDescription of privateObjective detected

--- src/main/java/projectIngSoft/App.java
+++ src/main/java/projectIngSoft/App.java
@@ -1,11 +1,14 @@
 package projectIngSoft;
 
+import projectIngSoft.PrivateObjectiveImpl.SfumatureBlu;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
@@ -13,13 +16,38 @@ public class App
 {
     public static void main( String[] args ) throws FileNotFoundException, Colour.ColorNotFoundException {
 
-        File file = new File("C:\\Users\\danie\\OneDrive - Politecnico di Milano\\IngInf_Mio\\Progetto - SWengi\\ing-soft-2018-paltenghi-parravicini-pellizzi\\src\\main\\test.txt");
+        //TODO Capire come mai il privateObjective non riesce a leggere la sua descrizione (ritorna null) problemi con classe astratta e metodi ereditati??
+        System.out.print("descrtiption -> " + new SfumatureBlu().getDescription() + "\n");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Use default configuration? y/n ");
+        String answer = scanner.next();
+        Game theGame;
+        if (answer.equals("y")) {
+            theGame = defaultGame();
+        } else {
+            try {
+                theGame = createGame();
+            } catch (InputMismatchException e) {
+                System.out.print("Mismatch Input... \n");
+                return;
+            }
+        }
+        if (theGame.isValid()) {
+            theGame.setupPhase();
+        } else {
+            System.out.print("Invalid Game created... \n");
+        }
+
+        /*
+        // OLD CODE
+        File file = new File("src/main/test.txt");
         WindowPattern window = new WindowPattern(new Scanner(file));
 
         System.out.println(window);
 
         Die myDie = new Die(0, Colour.RED);
-        System.out.println(myDie);
+        //System.out.println(myDie);
 
         ArrayList<Die> diceBag = new ArrayList<Die>();
         for (Colour c : Colour.validColours()){
@@ -29,9 +57,38 @@ public class App
                 diceBag.add(newDie.rollDie());
             }
         }
-
-
-
         System.out.println(diceBag);
+        */
+    }
+
+    public static Game defaultGame(){
+        Game theGame = new Game(3);
+        theGame.add(new Player("Matteo"));
+        theGame.add(new Player("Daniele"));
+        theGame.add(new Player("Kris"));
+        return theGame;
+    }
+
+    public static Game createGame() throws InputMismatchException{
+        // create a scanner so we can read the command-line input
+        Scanner scanner = new Scanner(System.in);
+        //  prompt for the number of players
+        int nPlayers;
+        System.out.print("Enter the number of players: ");
+        nPlayers = scanner.nextInt();
+
+        // create the game
+        Game theGame = new Game(nPlayers);
+        System.out.println("New Game successfully created!");
+
+        // loop for each player
+        for (int i = 1; i <= nPlayers; i++) {
+            //  prompt for the player name
+            System.out.println(String.format("Name for player nr %d : ", i));
+            String newName = scanner.next();
+            Player newPlayer = new Player(newName);
+            theGame.add(newPlayer);
+        }
+        return theGame;
     }
 }
--- src/main/java/projectIngSoft/Game.java
+++ src/main/java/projectIngSoft/Game.java
@@ -1,31 +1,108 @@
 package projectIngSoft;
 
+import projectIngSoft.PrivateObjectiveImpl.*;
+import projectIngSoft.PublicObjectiveImpl.*;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Game {
 
     private final int numPlayers;
+    private ArrayList<Player> players;
     private final ArrayList<Die> diceBag;
     private final ArrayList<Die> draftPool;
     private final RoundTracker rounds;
-    private final ArrayList<Card> publicObjectives;
+    private ArrayList<Card> privateObjectives;
+    private ArrayList<Card> publicObjectives;
+    private ArrayList<WindowPattern> windowPatterns;
 
-    public Game(int players) {
-        numPlayers = players;
+    /*
+    @requires theNumOfPlayer > 0
+    @ensures
+        (* everything is initialized *)
+    */
+    public Game(int theNumOfPlayer) {
+        // set required number of players for this game
+        numPlayers = theNumOfPlayer;
+        // initialize emplty list of player
+        players = new ArrayList<Player>();
+        // create dies and populate Die Bag
         diceBag = new ArrayList<Die>();
-        draftPool = new ArrayList<Die>();
-        rounds = new RoundTracker();
-        publicObjectives = new ArrayList<Card>();
-        // populate Die Bag
-        for (Colour c : Colour.values()){
-            for(int i = 1; i <= 18; i++){
+        for (Colour c : Colour.values()) {
+            for (int i = 1; i <= 18; i++) {
                 Die newDie = new Die(c);
                 this.diceBag.add(newDie);
             }
         }
+        // initialize empty draft pool
+        draftPool = new ArrayList<Die>();
+        // initialize Rount Tracker obj
+        rounds = new RoundTracker();
+        // initialize private Objective cards
+        privateObjectives = new ArrayList<Card>();
+        privateObjectives.add(new SfumatureBlu());
+        privateObjectives.add(new SfumatureGialle());
+        privateObjectives.add(new SfumatureRosse());
+        privateObjectives.add(new SfumatureVerdi());
+        privateObjectives.add(new SfumatureViola());
+        // initialize public Objective cards
+        publicObjectives = new ArrayList<Card>();
+        publicObjectives.add(new ColoriDiversiColonna());
+        publicObjectives.add(new ColoriDiversiRiga());
+        publicObjectives.add(new DiagonaliColorate());
+        publicObjectives.add(new SfumatureChiare());
+        publicObjectives.add(new SfumatureDiverse());
+        publicObjectives.add(new SfumatureDiverseColonna());
+        publicObjectives.add(new SfumatureDiverseRiga());
+        publicObjectives.add(new SfumatureMedie());
+        publicObjectives.add(new SfumatureScure());
+        publicObjectives.add(new VarietaColore());
+        // initialize window pattern cards
+        windowPatterns = new ArrayList<WindowPattern>();
+        // Shuffle everything
+        Collections.shuffle(diceBag);
+        Collections.shuffle(publicObjectives);
+        Collections.shuffle(privateObjectives);
+        // remove cards and leave only 3 publicObjective card for the game
+        while (publicObjectives.size() > 3) publicObjectives.remove(0);
     }
 
+    /*
+    @ensures
+        getNumberOfPlayers() == old(getNumberOfPlayers()) + 1  &&
+        (* newPlayer has been added to the list of players *)
+    */
+    public void add(Player newPlayer) {
+        if (players.size() < numPlayers) {
+            players.add(newPlayer);
+            System.out.println("New player added: " + newPlayer.getName());
+        }
+    }
+
+    /*
+    @assignable nothing
+    @ensures \result == (* number of players enrolled at the game now*)
+    */
+    public int getNumberOfPlayers(){
+        return players.size();
+    }
+
+    public boolean isValid() {
+        return players.size() == numPlayers;
+    }
+
+    public void setupPhase(){
+        for (Player p : players) {
+            PrivateObjective randomPrivateObjective = (PrivateObjective) privateObjectives.remove(0);
+            p.setMyPrivateObjective(randomPrivateObjective);
+            //TODO Capire come mai il privateObjective non riesce a leggere la sua descrizione (ritorna null) problemi con classe astratta e metodi ereditati??
+            System.out.println("To " + p.getName() + " has been given : " + p.getMyPrivateObjective());
+            System.out.println("To " + p.getName() + " has been given : " + p.getMyPrivateObjective().getDescription());
+        }
+
+    }
 
 
 }
