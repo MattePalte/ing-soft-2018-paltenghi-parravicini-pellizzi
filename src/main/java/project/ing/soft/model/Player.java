91b1ffdc1ad45cfc07bf0c023442c504555981dc
krijo <10522862@polimi.it>
2018-06-09 15:16:06 +0200

    Added javadoc to Player

--- src/main/java/project/ing/soft/model/Player.java
+++ src/main/java/project/ing/soft/model/Player.java
@@ -18,6 +18,10 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.util.*;
 
+/**
+ * The Player class is a representation of a player in the game. It contains all the
+ * information about the player itself
+ */
 public class Player implements Serializable{
     private final String            name;
 
@@ -37,6 +41,14 @@ public class Player implements Serializable{
 
     //In order to let the player use patterns with non-predefined dimensions
     //the player placedDice matrix is created when patternCard is chosen.
+
+    /**
+     * Default Player constructor. It saves player's name and a reference to its view.
+     * In order to let the player use patterns with non-predefined dimensions
+     * the player "placedDice" matrix is created when the patternCard is chosen.
+     * @param name of the player
+     * @param aView reference to the player's view
+     */
     public Player(String name, IView aView) {
         this.name                    = name;
         this.isPatternFlipped        = false;
@@ -52,6 +64,13 @@ public class Player implements Serializable{
 
     // the only field that is actually copied is the placedDie matrix.
     // that is due to the fact that other fields are immutable objects
+
+    /**
+     * Player productor. It takes a Player instance as a parameter and copy its information.
+     * The only field that is actually copied is the "placedDie" matrix.
+     * This is due to the fact that other fields are immutable objects
+     * @param pToBeCopied player whose information will be copied
+     */
     public Player(Player pToBeCopied) {
         this.name                    = pToBeCopied.name;
         this.isPatternFlipped        = pToBeCopied.isPatternFlipped;
@@ -66,6 +85,13 @@ public class Player implements Serializable{
         this.isConnected = pToBeCopied.isConnected;
     }
 
+    /**
+     * Player productor. This specific method is used only if the player asks for a reconnection
+     * before the match started. In that case it's necessary to copy all player information,
+     * but the view reference must be changed
+     * @param pToBeCopied player whose information will be copied
+     * @param view new reference to the player's view
+     */
     public Player(Player pToBeCopied, IView view){
         this(pToBeCopied);
         this.myView = view;
@@ -73,10 +99,20 @@ public class Player implements Serializable{
 
 
     //region memento
+
+    /**
+     * Method which creates a memento of the player by copying its information
+     * @return a player memento
+     */
     public Player getMemento(){
         return new Player(this);
     }
 
+    /**
+     * Method which saves information contained in a player memento to the player's
+     * current state
+     * @param p player whose information are saved in the player's current state
+     */
     public void saveMemento(Player p){
         if(this.name.equals(p.name) ){
             this.isPatternFlipped        = p.isPatternFlipped;
@@ -94,19 +130,37 @@ public class Player implements Serializable{
 
 
     //region setter
+
+    /**
+     * Setter of the pattern card needed to let player choose its pattern
+     * @param givenPatternCards list of 2 pattern cards
+     */
     public void givePossiblePatternCard(List<WindowPatternCard> givenPatternCards) {
         possiblePatternCards = new ArrayList<>(givenPatternCards);
     }
 
+    /**
+     * Setter of the pattern card. Used to save player's choice about the pattern card
+     * @param aWindowPatternCard the pattern card chosen by the player
+     */
     public void setPatternCard(WindowPatternCard aWindowPatternCard) {
         this.myWindowPatternCard = (aWindowPatternCard);
         this.placedDice = new Die[getPattern().getHeight()][getPattern().getWidth()];
     }
 
+    /**
+     * Setter of a boolean flag which indicates if the player chose the front or the rear
+     * side of the pattern card
+     * @param patternFlipped a flag to indicate which side of the pattern the player chose
+     */
     public void setPatternFlipped(boolean patternFlipped) {
         isPatternFlipped = patternFlipped;
     }
 
+    /**
+     * Setter of the player's private objective
+     * @param myPrivateObjective the private objective given to the player
+     */
     public void setPrivateObjective(PrivateObjective myPrivateObjective) {
         this.myPrivateObjective = myPrivateObjective;
     }
@@ -114,36 +168,70 @@ public class Player implements Serializable{
     //endregion
 
     //region getter
+
+    /**
+     * @return a boolean flag which indicates if the player is still connected
+     */
     public boolean isConnected() {
         return myView != null && isConnected;
     }
-    //@assignable nothing
+
+    /**
+     * Getter of the player's name
+     * @return the name of the player
+     */
     public String getName() {
         return name;
     }
-    //@assignable nothing
+
+    /**
+     * Getter of the list of possible pattern cards the player can choose
+     * @return a copy of the list of PossiblePatternCards
+     */
     public List<WindowPatternCard> getPossiblePatternCard(){
         return new ArrayList<>(possiblePatternCards);
     }
-    //@assignable nothing
+
+    /**
+     * Getter of the player's pattern card
+      * @return the pattern card bound to the player
+     */
     public WindowPatternCard getPatternCard() {
         return myWindowPatternCard;
     }
-    //@assignable nothing
+
+    /**
+     * Getter of the pattern chosen by the player
+     * @return the pattern chosen by the player
+     */
     public WindowPattern getPattern(){
         if(myWindowPatternCard == null)
             return null;
         return !isPatternFlipped ? myWindowPatternCard.getFrontPattern() : myWindowPatternCard.getRearPattern();
     }
-    //@assignable nothing
+
+
+    /**
+     * Getter of player's private objective
+     * @return player's private objective
+     */
     public PrivateObjective getPrivateObjective() {
         return myPrivateObjective;
     }
-    //@assignable nothing
+
+    /**
+     * Getter of player's placed dice matrix
+     * @return a copy of the matrix with the dice placed by the player in the game
+     */
     public Die[][] getPlacedDice(){
         return  cloneArray(placedDice);
     }
-    //@assignable nothing
+
+    /**
+     * Getter of a die placed on the player's placedDice matrix in the given position
+     * @param c a position of the placedDice matrix
+     * @return the die placed in the given position
+     */
     public Die getPlacedDice(Coordinate c){
         Die aDie = placedDice[c.getRow()][ c.getCol()];
         if(aDie != null)
@@ -166,6 +254,10 @@ public class Player implements Serializable{
         return target;
     }
 
+    /**
+     * @return a boolean flag which indicates whether the player has already placed a die
+     * in this turn or not
+     */
     public boolean getHasPlacedADieInThisTurn(){
         return hasPlacedADieInThisTurn;
     }
@@ -175,6 +267,9 @@ public class Player implements Serializable{
     }
 //endregion
 
+    /**
+     * @return the points earned by the player completing its private objective
+     */
     public int countPrivateObjectivesPoints(){
         return myPrivateObjective.countPoints(this);
     }
@@ -183,6 +278,12 @@ public class Player implements Serializable{
 
     //region die placement
 
+    /**
+     * Places a die in the given position of the placedDice matrix
+     * @param aDie the die to be placed
+     * @param row the row index of the position on which the die must be placed
+     * @param col the column index of the position on which the die must be placed
+     */
     private void placeDieWithoutConstraints(Die aDie, int row, int col){
         placedDice[row][col] =    new Die(aDie);
         hasPlacedADieInThisTurn = true;
@@ -194,6 +295,21 @@ public class Player implements Serializable{
     //      a die is placed in an occupied position -> PositionOccupiedException
     //      a die placed doesn't match rules -> RuleViolatedExceptions
     //      a pattern constraint is violated -> PatternConstraintViolatedException
+
+    /**
+     * Places a die in the given position. Before placing it in the placedDice matrix,
+     * executes some checks about rules, constraints and dice positions
+     * @param aDie the die to be placed
+     * @param row the row index of the position on which the die must be placed
+     * @param col the column index of the position on which the die must be placed
+     * @param checkPresence a boolean flag which indicates if there must be a die in
+     *                      positions adjacent to the given one
+     * @throws PositionOccupiedException if a die is asked to be placed in an occupied
+     * position
+     * @throws PatternConstraintViolatedException the constraint in the given position is
+     * not compatible with the die asked to be placed
+     * @throws RuleViolatedException if the player is trying to place a die without respecting rules
+     */
     public void placeDie(Die aDie, int row, int col, boolean checkPresence) throws PositionOccupiedException, PatternConstraintViolatedException, RuleViolatedException {
 
         if(getHasPlacedADieInThisTurn())
@@ -205,6 +321,24 @@ public class Player implements Serializable{
 
     }
 
+    /**
+     * Moves some dice from the start positions to the end positions. Before move them in the placedDice matrix,
+     * executes some checks about rules, constraints and dice positions
+     * @param start list of starting move positions
+     * @param end list of ending move positions
+     * @param checkColour a boolean flag which indicates whether the move must respect
+     *                    colour restrictions or not
+     * @param checkValue a boolean flag which indicates whether the move must respect value
+     *                   restrictions or not
+     * @param checkPresence a boolean flag which indicates if there must be a die in
+     *                      positions adjacent to the given one
+     * @throws RuleViolatedException if the player is trying to move dice without
+     * respecting rules
+     * @throws PatternConstraintViolatedException if the player is trying to move dice
+     * without respecting some constraint on its pattern
+     * @throws PositionOccupiedException if the player is trying to move dice in already occupied
+     * positions
+     */
     public void moveDice(List<Coordinate> start, List<Coordinate> end, boolean checkColour, boolean checkValue, boolean checkPresence) throws RuleViolatedException, PatternConstraintViolatedException, PositionOccupiedException {
         Player copyTest = this.getMemento();
         List<Die> queue = new ArrayList<>();
@@ -228,6 +362,12 @@ public class Player implements Serializable{
         this.saveMemento(copyTest);
     }
 
+    /**
+     * Removes a die from the placedDice matrix
+     * @param row the row index of the position from which the die must be removed
+     * @param col the column index of the position from which the die must be removed
+     * @return the die removed from the given position
+     */
     private Die removeDie(int row, int col){
         Die ret;
         ret = placedDice[row][col];
@@ -235,10 +375,21 @@ public class Player implements Serializable{
         return ret;
     }
 
+    /**
+     * Removes a die from the placedDice matrix
+     * @param c coordinate containing row and column indexes of the position from which the
+     *          die must be removed
+     * @return the die removed from the given position
+     */
     private Die removeDie(Coordinate c){
         return removeDie(c.getRow(), c.getCol());
     }
 
+    /**
+     * Gets the positions compatible for the given die to be placed
+     * @param aDie the die asked to be placed
+     * @return a list of compatible coordinate with the given die
+     */
     public List<Coordinate> getCompatiblePositions(Die aDie){
         ArrayList<Coordinate> ret = new ArrayList<>();
         if(getHasPlacedADieInThisTurn())
@@ -258,6 +409,23 @@ public class Player implements Serializable{
         return ret;
     }
 
+    /**
+     * Executes checks about dice already placed on the placedDice matrix, rules and constraints
+     * @param aDie the die asked to be placed or moved
+     * @param row the row index of the position on which the die is asked to be placed or moved
+     * @param col the column index of the position on which the die is asked to be placed or moved
+     * @param checkColor a boolean flag which indicates whether the asked move must respect colour
+     *                   restrictions or not
+     * @param checkValue a boolean flag which indicates whether the asked move must respect value
+     *                   restrictions or not
+     * @param checkPresence a boolean flag which indicates if a die must be placed in
+     *                      positions adjacent to the given one
+     * @throws RuleViolatedException if the player asked a move which violates some rule
+     * @throws PatternConstraintViolatedException if the player asked for a move which is
+     * not compatible with a pattern constraint
+     * @throws PositionOccupiedException if the player asked for a move which involves to
+     * place a die in a already occupied position
+     */
     private void checkPlaceDie(Die aDie, int row, int col, boolean checkColor, boolean checkValue, boolean checkPresence) throws RuleViolatedException, PatternConstraintViolatedException, PositionOccupiedException {
 
         if(placedDice[row][col]!= null) {
@@ -301,10 +469,35 @@ public class Player implements Serializable{
 
     }
 
+
+    /**
+     * Executes checks about dice already placed on the placedDice matrix, rules and constraints
+     * @param aDie the die asked to be placed or moved
+     * @param c the coordinate containing row and column indexes of the position
+     *          on which the die is asked to be placed or moved
+     * @param checkColor a boolean flag which indicates whether the asked move must respect colour
+     *                   restrictions or not
+     * @param checkValue a boolean flag which indicates whether the asked move must respect value
+     *                   restrictions or not
+     * @param checkPresence a boolean flag which indicates if a die must be placed in
+     *                      positions adjacent to the given one
+     * @throws RuleViolatedException if the player asked a move which violates some rule
+     * @throws PatternConstraintViolatedException if the player asked for a move which is
+     * not compatible with a pattern constraint
+     * @throws PositionOccupiedException if the player asked for a move which involves to
+     * place a die in a already occupied position
+     */
     private void checkPlaceDie(Die aDie, Coordinate c, boolean checkColor, boolean checkValue, boolean checkPresence) throws RuleViolatedException, PatternConstraintViolatedException, PositionOccupiedException {
         checkPlaceDie(aDie, c.getRow(), c.getCol(), checkColor, checkValue, checkPresence);
     }
 
+    /**
+     * Gets a list of the dice placed in positions adjacent to the given one
+     * @param placedDice the placedDice matrix of the player
+     * @param row the row index of the position asked to be looked for adjacent dice
+     * @param col the column index of the position asked to be looked for adjacent dice
+     * @return a list of the dice placed in positions adjacent to the given one
+     */
     private ArrayList<Die> getOrthogonalAdjacents(Die[][] placedDice, int row, int col){
         ArrayList<Die> ret = new ArrayList<>();
 
@@ -320,6 +513,14 @@ public class Player implements Serializable{
         return ret;
     }
 
+    /**
+     * Check if there is a die in a position adjacent to the given one. This methods check if
+     * there is a die also on diagonals
+     * @param row the row index of the position asked to be looked for adjacent dice
+     * @param col the column index of the position asked to be looked for adjacent dice
+     * @return a boolean flag which indicates whether there is a die in a position adjacent
+     * to the given one or not
+     */
     public boolean isThereAnAdjacentDie(int row, int col)  {
         for (int deltaRow = -1; deltaRow <= 1 ; deltaRow++) {
             for (int deltaCol = -1; deltaCol <= 1; deltaCol++) {
@@ -334,6 +535,10 @@ public class Player implements Serializable{
         return false;
     }
 
+    /**
+     *
+     * @return the number of empty cells in the player's placedDice matrix
+     */
     public int getEmptyCells(){
         int ret = 0;
 
@@ -344,12 +549,19 @@ public class Player implements Serializable{
         return ret;
     }
 
+    /**
+     * Re-initializes player's flag which indicates if it placed a die in the turn or not
+     */
     public void endTurn() {
         hasPlacedADieInThisTurn = false;
     }
 
     //endregion
 
+    /**
+     * Sends events to player's view
+     * @param events list of events to be sent to player's view
+     */
     public void update(Event... events) {
         try {
             if(isConnected())
@@ -365,6 +577,10 @@ public class Player implements Serializable{
 
     //region connection
 
+    /**
+     * Sets to false the flag isConnected, interrupts the execution of player's view proxy
+     * and sets player's view reference to null
+     */
     public void disconnectView(){
         isConnected = false;
         if(myView != null)
@@ -372,6 +588,11 @@ public class Player implements Serializable{
         myView = null;
     }
 
+    /**
+     * Sets to true the flag isConnected. If the player is still connected, interrupts the
+     * execution of player's old view proxy and sets player's new view reference
+     * @param myView player's new view reference
+     */
     public void reconnectView(IView myView){
         isConnected = true;
         if(this.myView != null)
@@ -383,6 +604,12 @@ public class Player implements Serializable{
 
     //region object override
 
+    /**
+     *
+     * @param o player to be compared to this
+     * @return a boolean flag which indicates whether the given object's state is equal to
+     * player's state or not
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -423,6 +650,10 @@ public class Player implements Serializable{
                 "---------------------\n";
     }
 
+    /**
+     *
+     * @return a string representation of player's pattern
+     */
     private String stringifyPlayerGameBoard() {
         StringBuilder aBuilder = new StringBuilder();
         String tmp;
